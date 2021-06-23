6fc1c1d31df0fa6907b49970b18eb1062bcfd24d
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-06-23 18:06:46 -0500

    Agregando AlertDialog a boton eliminar usuario, aún falta llamar al método Delete de la API

--- app/src/main/java/com/elpunto/app/ui/miperfil/PerfilFragment.java
+++ app/src/main/java/com/elpunto/app/ui/miperfil/PerfilFragment.java
@@ -1,8 +1,10 @@
 package com.elpunto.app.ui.miperfil;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.elpunto.app.MainActivity;
 import com.elpunto.app.R;
 import com.elpunto.app.common.Constantes;
 import com.elpunto.app.common.SharedPreferencesManager;
@@ -40,6 +43,27 @@ public class PerfilFragment extends Fragment {
         Glide.with(getContext()).load(urlFoto).apply(RequestOptions.circleCropTransform()).into(binding.ivFotoPerfil);
         binding.tvNombres.setText("Nombres: "+nombres);
         binding.tvApellidos.setText("Apellidos: "+apellidos);
+        binding.btnEliminarUsuario.setOnClickListener(v -> {
+            mostrarDialogoEliminarCuenta();
+        });
         return vista;
+    }
+
+    public void mostrarDialogoEliminarCuenta() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Eliminar cuenta ):");
+        builder.setMessage("Estás seguro de eliminar tu cuenta? Después no podrás volver a usarla...")
+                .setPositiveButton("Sí, estoy seguro", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Aun no elimina el usuario, solo cierra la app...
+                        getActivity().finishAffinity();
+                    }
+                }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).show();
     }
 }
