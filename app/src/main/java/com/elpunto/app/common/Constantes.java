9636ecf56d6ea413e800a65435bee2918d551633
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-08-21 12:39:38 -0500

    Modificando constantes para conexion con wifi o cable, debo hostear la API xdd

--- app/src/main/java/com/elpunto/app/common/Constantes.java
+++ app/src/main/java/com/elpunto/app/common/Constantes.java
@@ -1,12 +1,18 @@
 package com.elpunto.app.common;
 
 public class Constantes {
-    public static final String URL_BASE_API ="http://192.168.0.15:9898/api";
-    public static final String URL_LOGIN = "http://192.168.0.15:9898/api/usuarios/login";
-    public static final String URL_REGISTRO = "http://192.168.0.15:9898/api/usuarios/registro";
-    public static final String URL_BASE_CATEGORIAS = "http://192.168.0.15:9898/api/categorias/";
-    public static final String URL_FOTO_PRODUCTO = "http://192.168.0.15:9898/api/productos/foto/";
 
+    //Con cable
+    public static final String URL_BASE = "http://192.168.0.15:9898";
+
+    //Con wifi
+    //public static final String URL_BASE = "http://192.168.0.19:9898";
+
+    public static final String URL_BASE_API =URL_BASE+"/api";
+    public static final String URL_LOGIN = URL_BASE+"/api/usuarios/login";
+    public static final String URL_REGISTRO = URL_BASE+"/api/usuarios/registro";
+    public static final String URL_BASE_CATEGORIAS = URL_BASE+"/api/categorias/";
+    public static final String URL_FOTO_PRODUCTO = URL_BASE+"/api/productos/foto/";
 
     //SharedPreferences
     public static final String PREF_ID = "PREF_ID";
